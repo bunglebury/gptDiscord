f16c384210861dfd327f4ff5c79b31cbf37bf596
Jason T <110324928+bunglebury@users.noreply.github.com>
2023-05-09 23:30:12 -0700

    Update disBot.py

--- disBot.py
+++ disBot.py
@@ -2,7 +2,7 @@ import openai
 from discord.ext import commands
 import discord
 from dotenv import dotenv_values
-from keep_alive import keep_alive
+
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
@@ -15,8 +15,10 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     print(f'{bot.user} has awakened')
 
+messages = [{"role": "system", "content": "you are a rude and sarcastic chatbot"}]
 @bot.command()
 async def gpt(ctx, *, query):
+    messages.append({"role": "user", "content": query})
     print(query)
     output = openai.Completion.create(
         engine="text-davinci-003",
@@ -24,8 +26,21 @@ async def gpt(ctx, *, query):
         temperature=1,
         max_tokens=100,
     )
+   
+
     text = output['choices'][0]['text']
+    messages.append({"role": "system", "content": text})
     print(text)
     await ctx.send(text)
 
+@bot.command()
+async def img(ctx,query):
+    res = openai.Image.create(
+        prompt=query,
+        n=1,
+        size="512x512"
+    )
+    data = res["data"][0]["url"]
+    print(data)
+    await ctx.send(data)
 bot.run(config["DISCORD_BOT_KEY"])
